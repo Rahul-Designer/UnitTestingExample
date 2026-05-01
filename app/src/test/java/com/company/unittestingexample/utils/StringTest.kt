044664ff07fd70457b14f0a9d0fd0bc0df7b5c45
Rahul Singh <singhrahul50888@gmail.com>
2026-05-01 12:33:39 +0530

    Change add parameterized testcase in reverse string

--- app/src/test/java/com/company/unittestingexample/utils/StringTest.kt
+++ app/src/test/java/com/company/unittestingexample/utils/StringTest.kt
@@ -2,6 +2,8 @@ package com.company.unittestingexample.utils
 
 import org.junit.Assert
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
 
 class StringTest {
 
@@ -27,6 +29,28 @@ class StringTest {
     fun testStringNullInput_expectedException(){
         val sut = Utils()
         val result = sut.reverseString(null)
+    }
+}
 
+@RunWith(Parameterized::class)
+class ParameterizedReverseStringTest(val input : String, val expectedValue : String){
+
+    @Test
+    fun test(){
+        val sut = Utils()
+        val result = sut.reverseString(input)
+        Assert.assertEquals(expectedValue,result)
+    }
+
+    companion object{
+
+        @JvmStatic
+        @Parameterized.Parameters(name = "{index} : {0} is reverseString - {1}")
+        fun checkReverseString() : List<Array<Any>> {
+            return listOf(
+                arrayOf("   ","   "),
+                arrayOf("abc","cba")
+            )
+        }
     }
 }
